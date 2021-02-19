75a4ebd8e1e545408c9d7d7de1042a9e41702f62
GeorgiAndonov <Civilianboy3@gmail.com>
2021-02-19 05:10:32 +0200

    Added ROS workspace - check out the ROS tutorial on WeBots

---
+++ SDP/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/georgi/shoefy/SDP/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/georgi/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/georgi/catkin_ws/devel;/opt/ros/noetic' != '' else []
