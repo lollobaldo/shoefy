53ceb6c3c68eebc5b7a18f6ff55536b1d677c71b
Min.S <32324812+Zspirates96@users.noreply.github.com>
2021-02-27 04:24:02 +0000

    Add files via upload

--- SDP/controllers/robot/movement.py
+++ SDP/controllers/robot/movement.py
@@ -1,55 +1,15 @@
 from controller import Robot
 import math as Math
+import tkinter as tk
+from tkinter import simpledialog
 
 class Movement():
-    def __init__(self, ds, wheels, gps, speed, kb):
-        self.ds = ds
+    def __init__(self, wheels, speed):
         self.wheels = wheels
-        self.gps = gps #not done
         self.speed = speed
-        self.kb = kb
-        self.coord = [] #not done
-        self.auto = False
-        
-    def distance(self, dest): #not done
-        self.coord = self.gps.getValues()
-        ans = (dest[0] - self.coord[0])**2 + (dest[1] - self.coord[2])**2
-        return Math.sqrt(ans)
-        
-    def angle(self, dest): #not done
-        self.coord = self.gps.getValues()
-        if dest[0] - self.coord[0] == 0:
-            return 0
-        ans = (dest[1] - self.coord[2])/(dest[0]-self.coord[0])
-        ans = Math.degrees(Math.atan(ans))
-        return ans
     
-    def move(self):
-        obstacle = [0,0]
-        for i in range(2):  
-            if self.ds[i].getValue() < 1000:
-                obstacle[i] = 1      
-        if obstacle[0] == 1 and obstacle[1] == 1:
-            obstacle = [0,0]
-            for i in range(2):  
-                if self.ds[i+2].getValue() < 1000:
-                    obstacle[i] = 1
-            if obstacle == [0,0]:
-                obstacle[0] = 1
-        if obstacle[0] == 1:
-            self.left()
-            return
-        elif obstacle[1] == 1:
-            self.right()
-            return
-        self.forward()
-    
-    def control(self):
-        if self.auto:
-            self.move()
-        else:
-            self.stop()
-        key = self.kb.getKey()
+    def control(self, key):
+        self.stop()
         if key == 314:
             self.left()
         elif key == 316:
@@ -57,28 +17,24 @@ class Movement():
         elif key == 315:
             self.forward()
         elif key == 317:
-            self.auto = not self.auto
+            self.back()
         
     def left(self):
-        self.wheels[0].setVelocity(-self.speed/2)
-        self.wheels[1].setVelocity(-self.speed/2)
-        self.wheels[2].setVelocity(self.speed/2)
-        self.wheels[3].setVelocity(self.speed/2)
+        self.wheels[0].setVelocity(self.speed)
+        self.wheels[1].setVelocity(-self.speed)
         
     def right(self):
-        self.wheels[0].setVelocity(self.speed/2)
-        self.wheels[1].setVelocity(self.speed/2)
-        self.wheels[2].setVelocity(-self.speed/2)
-        self.wheels[3].setVelocity(-self.speed/2)
+        self.wheels[0].setVelocity(-self.speed)
+        self.wheels[1].setVelocity(self.speed)
     
     def forward(self):
         self.wheels[0].setVelocity(self.speed)
         self.wheels[1].setVelocity(self.speed)
-        self.wheels[2].setVelocity(self.speed)
-        self.wheels[3].setVelocity(self.speed)
+        
+    def back(self):
+        self.wheels[0].setVelocity(-self.speed)
+        self.wheels[1].setVelocity(-self.speed)
         
     def stop(self):
         self.wheels[0].setVelocity(0)
         self.wheels[1].setVelocity(0)
-        self.wheels[2].setVelocity(0)
-        self.wheels[3].setVelocity(0)
--- SDP/controllers/robot/robot.py
+++ SDP/controllers/robot/robot.py
@@ -1,30 +1,97 @@
 from controller import Robot
 from controller import PositionSensor
 import movement as mv
+import math
 
-TIME_STEP = 64
-robot = Robot()
+def run_robot(robot):
+    TIME_STEP = 64
 
-ds = []
-dsNames = ['dsR', 'dsL', 'dsSR', 'dsSL']
-for i in range(len(dsNames)):
-    ds.append(robot.getDevice(dsNames[i]))
-    ds[i].enable(TIME_STEP)
+    ps = []
+    psNames = ['ps1', 'ps2']
+    for i in range(len(psNames)):
+        ps.append(robot.getDevice(psNames[i]))
+        ps[i].enable(TIME_STEP)
     
-wheels = []
-wheelsNames = ['wheelLF', 'wheelLR', 'wheelRF', 'wheelRR']
-for i in range(len(wheelsNames)):
-    wheels.append(robot.getDevice(wheelsNames[i]))
-    wheels[i].setPosition(float('inf'))
-    wheels[i].setVelocity(0.0)
+    wheels = []
+    wheelsNames = ['wheel1', 'wheel2']
+    for i in range(len(wheelsNames)):
+        wheels.append(robot.getDevice(wheelsNames[i]))
+        wheels[i].setPosition(float('inf'))
+        wheels[i].setVelocity(0.0)
 
-gps = robot.getDevice('gps') #not done
-#gps.enable(64) #not done
+    kb = robot.getKeyboard()
+    kb.enable(64)
 
-kb = robot.getKeyboard()
-kb.enable(64)
+    m = mv.Movement(wheels, 10.0)
+    
+    #odometry
+    radius = 0.057
+    pos = [0,0,0]
+    last_psV = [0,0]
+    curr_psV = [0,0]
+    psV = [0,0]
+    diff = [0,0]
+    v = 0
+    w = 0
+    wheel_dist = 0.414
+    dt = 1
+    showPos = False
+    target = [4, 4]
+    #count = 0
+    #last_dist = 0 
+    
+    while robot.step(TIME_STEP) != -1:
+        
+        for i in range(len(psNames)):
+            curr_psV[i] = ps[i].getValue() * radius
+            diff[i] = curr_psV[i] - last_psV[i]
+            if diff[i] == 0.001:
+                diff[i] = 0
+                curr_psV[i] = last_psV[i]
+        v = (diff[0] + diff[1]) / 2
+        w = (diff[0] - diff[1]) / wheel_dist
+        
+        pos[2] += w * dt
+        #pos[2] %= 2*math.pi
+        vx = v * math.cos(pos[2])
+        vy = v * math.sin(pos[2])
+        pos[0] += vx * dt
+        pos[1] += vy * dt
+        
+        key = kb.getKey()
+        if key == 83:
+            showPos = not showPos
+        if showPos:
+            print("position: {}".format(pos))
+          
+        dist = distance([pos[0], pos[1]], target)
+        if dist > 1:
+            #if last_dist < dist:
+                #count+=1
+            angle = (target[1] - pos[1])/(target[0] - pos[0])
+            angle = math.atan(angle) - pos[2]
+            #if count == 2:
+                #pos[2] += math.pi
+                #count = 0
+            if angle > 0.1:
+                m.control(314)
+            elif angle < -0.1:
+                m.control(316)
+            else:
+                m.control(315)
+            #last_dist = dist
+        else:
+            m.control(1234567890)
+            print("reached!")
+        
+        for i in range(len(psNames)):
+            last_psV[i] = curr_psV[i]
 
-m = mv.Movement(ds, wheels, gps, 10.0, kb)
+def distance(source, target):
+    ans = (target[1] - source[1]) ** 2
+    ans += (target[0] - source[0]) ** 2
+    return math.sqrt(ans)
 
-while robot.step(TIME_STEP) != -1:
-    m.control()
+if __name__ == '__main__':
+    robot = Robot()
+    run_robot(robot)
