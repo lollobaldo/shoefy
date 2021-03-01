e3d61323a7c3590dbfdbb7786493d06987151080
Matthew Hynes <matthew@xereeto.co.uk>
2021-03-01 10:36:11 +0000

    will finish later today or tomorrow, but the structure is basically there

--- terminal_and_robot_controller/shoefyAPI/shoefy.py
+++ terminal_and_robot_controller/shoefyAPI/shoefy.py
@@ -5,6 +5,19 @@ class shoefy(object):
         self.key = key
 
     def getBooking(self,id):
-      response = session.get(self.url+'/booking/'+id)
-      return response.json()
-    
+        response = session.get(self.url+'/booking/'+id)
+        return response.json()
+   
+    def getShoes(self,size):
+        response = session.get(self.url+'/shoes/'+str(size))
+        return response.json()
+
+    def takeShoe(self,row,column):
+        response = requests.patch(self.url+'/shoe/'+str(row)+'/'+str(column),data="{ currentlyPresent: false }"
+        return response
+
+    def returnShoe(self,row,column):
+        response = requests.patch(self.url+'/shoe/'+str(row)+'/'+str(column),dat
+a="{ currentlyPresent: true }"
+        return response
+
---
+++ terminal_and_robot_controller/terminal.py
@@ -0,0 +1,22 @@
+import json
+import cv2
+from shoefyAPI import shoefy
+
+# need to specify domain and API key
+s = shoefy('api.shoefy.xereeto.co.uk','3924092384092')
+
+def readQr(image):
+    detector=cv2.QRCodeDetector()
+    data,bbox,straight_qrcode = detector.detectAndDecode(image)
+    if bbox is not None:
+        return data
+
+bookingData = s.getBooking(readQr(cv2.imread("sampleQR.png")))
+# check for time and date goes here
+
+print("Email: "+bookingData['email'])
+# then get the shoe size
+# look up the shoe size, get an available shoe and mark it unavailable
+# then send data to robot controller over tcp
+# simples
+# will sort functionality by idk like tomorrow
