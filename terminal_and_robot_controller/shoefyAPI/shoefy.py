ace1d85cabf8492523067667fc43e526a76a4036
Matthew Hynes <matthew@xereeto.co.uk>
2021-03-14 12:58:12 +0000

    Terminal now retrieves shoe data and sends to robot.

--- terminal_and_robot_controller/shoefyAPI/shoefy.py
+++ terminal_and_robot_controller/shoefyAPI/shoefy.py
@@ -1,23 +1,30 @@
 from . import session
 class shoefy(object):
-    def __init__(self,domain,key):
-        self.url = 'https://'+domain
-        self.key = key
+		def __init__(self,domain,key):
+				self.url = 'https://'+domain
+				self.key = key
 
-    def getBooking(self,id):
-        response = session.get(self.url+'/booking/'+id)
-        return response.json()
+		def getBooking(self,id):
+				response = session.get(self.url+'/booking/'+id)
+				return response.json()
    
-    def getShoes(self,size):
-        response = session.get(self.url+'/shoes/'+str(size))
-        return response.json()
+		def getShoes(self,size):
+				response = session.get(self.url+'/shoes/'+str(size))
+				return response.json()
+		
+		def getShoe(self,unit,row,column):
+				response = session.get(self.url+'/shoe/'+str(unit)+'/'+str(row)+'/'+str(column))
+				return response.json()
 
-    def takeShoe(self,row,column):
-        response = requests.patch(self.url+'/shoe/'+str(row)+'/'+str(column),data="{ currentlyPresent: false }"
-        return response
+		def takeShoe(self,unit,row,column):
+				response = session.patch(self.url+'/shoe/'+str(unit)+'/'+str(row)+'/'+str(column)+'/take')
+				return response
 
-    def returnShoe(self,row,column):
-        response = requests.patch(self.url+'/shoe/'+str(row)+'/'+str(column),dat
-a="{ currentlyPresent: true }"
-        return response
+		def replaceShoe(self,unit,row,column):
+				response = session.patch(self.url+'/shoe/'+str(unit)+'/'+str(row)+'/'+str(column)+'/replace')
+				return response
+		
+		def getAvailShoe(self,size):
+				response = session.get(self.url+'/shoe/'+str(size)+'/available')
+				return response.json()
 
--- terminal_and_robot_controller/terminal.py
+++ terminal_and_robot_controller/terminal.py
@@ -3,6 +3,12 @@
 import json
 import cv2
 from shoefyAPI import shoefy
+import socket
+
+ROBOT_IP = '127.0.0.1'
+ROBOT_PORT=1337
+BUFFER_SIZE=1024
+
 
 # need to specify domain and API key
 s = shoefy('api.shoefy.xereeto.co.uk','3924092384092')
@@ -17,6 +23,24 @@ bookingData = s.getBooking(readQr(cv2.imread("sampleQR.png")))
 # check for time and date goes here
 
 print("Email: "+bookingData['email'])
+paths=[]
+for size in bookingData['shoeSizes']:
+	sd=s.getAvailShoe(size);
+	paths.append(sd['path']);
+	print(s.takeShoe(sd['unit'],sd['row'],sd['column']));
+	print(s.getShoe(sd['unit'],sd['row'],sd['column']));
+	s.replaceShoe(sd['unit'],sd['row'],sd['column']);
+	print(s.getShoe(sd['unit'],sd['row'],sd['column']));
+
+print(paths)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((ROBOT_IP, ROBOT_PORT))
+for path in paths:
+		s.send(bytes(path,encoding='utf8'))
+		data=s.recv(BUFFER_SIZE)
+		print(data)
+s.close()
+	
 # then get the shoe size
 # look up the shoe size, get an available shoe and mark it unavailable
 # then send data to robot controller over tcp
